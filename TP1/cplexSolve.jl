1928300685d96772a830bb46c4d67bc30baa27ad
Yue0925 <mellet1997@outlook.com>
2022-02-24 17:36:07 +0100

    res TP1

--- TP1/cplexSolve.jl
+++ TP1/cplexSolve.jl
@@ -1,93 +1,121 @@
 TOL = 0.00001
 
 
-function cplexSolve()
-    include("ReserveNaturelles.txt")
+function cplexSolve(output::String)
+
+    fout = open(output, "w")
+
     lines = size(proba, 1)
-    probabilities = zeros(6, 10, 10)
+    probabilities = zeros(k, n, m)
     for l in 1:lines
         probabilities[round(Int, proba[l, 1]), round(Int, proba[l, 2]), round(Int, proba[l, 3])] = proba[l, 4]
     end
 
     M = Model(CPLEX.Optimizer) 
 
-    @variable(M, x[1:6, 1:10, 1:10], Bin) # x[k, i, j] = 1, if espece k is in the cell(i, j)
-    @variable(M, yc[1:10, 1:10], Bin) # yc[i, j] = 1, if cell(i, j) selected as zone central
-    @variable(M, yt[1:10, 1:10], Bin) # yt[i, j] = 1, if cell(i, j) selected as zone tampon
-
-
-    # each cell can be selected as at most one zone
-    for i in 1:10
-        for j in 1:10
-            @constraint(M, yc[i, j] + yt[i, j] <= 1) 
-        end
-    end
+    @variable(M, z[1:n, 1:m], Bin) # z[i, j] = 1, if cell(i, j) selected as zone central
+    @variable(M, x[1:n, 1:m], Bin) # x[i, j] = 1, if cell(i, j) is protected
     
     # apreantly, zone central cannot situates on the bord
-    @constraint(M, sum(yc[1, 1:10]) == 0)
-    @constraint(M, sum(yc[10, 1:10]) == 0)
-    @constraint(M, sum(yc[1:10, 1]) == 0)
-    @constraint(M, sum(yc[1:10, 10]) == 0)
+    @constraint(M, sum(z[1, 1:m]) == 0)
+    @constraint(M, sum(z[n, 1:m]) == 0)
+    @constraint(M, sum(z[1:n, 1]) == 0)
+    @constraint(M, sum(z[1:n, m]) == 0)
 
-    # each dangerours espece belongs to at least one zone
-    for i in 1:6
-        @constraint(M, sum(x[i, 1:10, 1:10]) >= 1)
-    end
 
-    for i in 1:10
-        for j in 1:10
-            # for each region
-            @constraint(M, 3 * yc[i, j] >= sum(x[k, i, j] for k in 1:3)) # dangerours espece
-            @constraint(M, 3 * (yc[i, j] + yt[i, j]) >= sum(x[k, i, j] for k in 4:6)) #commune espece
-            # for k in 1:3
-            #     @constraint(M, yc[i, j] >= x[k, i, j])
-            # end
-            # for k in 4:6
-            #     @constraint(M, yc[i, j] + yt[i, j] >= x[k, i, j])
-            # end
+    # a central plot is surrounded by protected plots
+    @constraint(M, [i in 2:n-1, j in 2:m-1], z[i, j] >= sum(x[i_, j_] for i_ in i-1:i+1, j_ in j-1:j+1) - 8)
+
+    # a plot cannot be selected as central zone if at least one of its neighbour is not protected
+    for i in 2:n-1
+        for j in 2:m-1
+            @constraint(M, [i_ in i-1:i+1, j_ in j-1:j+1], z[i, j] <= x[i_, j_])
         end
+        
     end
+
     
     # the survival probability constraint
-    for k in 1:6
-        @constraint(M, sum(log(1 - probabilities[k, i, j]) *  x[k, i, j] for i in 1:10 for j in 1:10) <= log(1 - alpha[k]))
-    end
+    @constraint(M, [e in 1:p], sum(log(1 - probabilities[e, i, j]) * z[i, j] for i in 1:n, j in 1:m) <= log(1-alpha[e]))
+    @constraint(M, [e in p+1:k], sum(log(1 - probabilities[e, i, j]) * x[i, j] for i in 1:n, j in 1:m) <= log(1-alpha[e]))
 
-    # the geographical constraint
-    for i in 2:9
-        for j in 2:9
-            @constraint(M, yc[i, j] >= sum(yc[p, q] + yt[p, q] for p in i-1:i+1 for q in j-1:j+1 if p!=i && q!=j) - 7)
-        end
-    end
 
-    @objective(M, Min, sum(c[i][j] * (yc[i, j]+yt[i, j]) * 10 for i in 1:10 for j in 1:10))
+    @objective(M, Min, sum(c[i][j] * x[i, j] for i in 1:n, j in 1:m))
 
-    start = time()
-    # solve the problem
+    # resolve
     optimize!(M)
 
-    computationTime = time() - start
     exploredNodes = MOI.get(backend(M), MOI.NodeCount())
+    GAP = MOI.get(M, MOI.RelativeGap())
+    solveTime = MOI.get(M, MOI.SolveTime())
+
     # status of model
     status = termination_status(M)
     isOptimal = status==MOI.OPTIMAL
 
-    centrals = Array{Tuple{Int64, Int64}, 1}()
-    tampons = Array{Tuple{Int64, Int64}, 1}()
 
-    println("isOptimal ? ", isOptimal)
-    if isOptimal
-        println("objective value : ", objective_value(M))
-        for i in 1:10
-            for j in 1:10
-                if JuMP.value(yc[i, j]) > TOL
-                    append!(centrals, (i, j))
-                end
-                if JuMP.value(yt[i, j]) > TOL
-                    append!(tampons, (i, j))
-                end
+    probability = zeros((k))
 
+    println(fout, "isOptimal ? ", isOptimal)
+    println(fout, "GAP : ", GAP)
+    println(fout, "exploredNodes : ", exploredNodes)
+    println(fout, "solveTime : ", round(solveTime, digits = 2))
+    println(fout, "objective value : ", round(Int, objective_value(M)))
+
+    if has_values(M)
+
+        # proba of each espèce
+        for e in 1:p
+            π = 1
+            for i in 1:n
+                for j in 1:m
+                    if JuMP.value(z[i, j]) > TOL
+                        π *= 1 - probabilities[e, i, j]
+                    end
+                end
             end
+            probability[e] = round(1 - π, digits = 2)
+        end
+
+        for e in p+1:k
+            π = 1
+            for i in 1:n
+                for j in 1:m
+                    if JuMP.value(x[i, j]) > TOL
+                        π *= 1 - probabilities[e, i, j]
+                    end
+                end
+            end
+            probability[e] = round(1 - π, digits = 2)
+        end
+
+        println(fout, "probability : ", probability)
+        println(fout)
+
+        # display
+        for i in 1:n
+            for j in 1:m
+                if JuMP.value(z[i, j]) > TOL
+                    print(fout, '■')
+                elseif JuMP.value(x[i, j]) > TOL
+                    print(fout, '▪') 
+                else
+                    print(fout, '□')
+                end
+            end
+            println(fout)
         end
     end
+
+    close(fout)
+end
+
+
+
+function run()
+    file = "ReserveNaturelles.txt"
+    include(file)
+
+    output = "res/cas4"
+    cplexSolve(output)
 end
