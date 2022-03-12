9308401a1dac01c3c7404edf958907aa4ee9ec95
Yue0925 <mellet1997@outlook.com>
2022-03-12 14:00:00 +0100

    analysis interval r

--- TP5/ULS.jl
+++ TP5/ULS.jl
@@ -1,23 +1,31 @@
 
 using Random
-using PyPlot
+import PyPlot; const plt = PyPlot
+
+"""
+tape in julia terminal : 
+
+ENV["PYTHON"]=""
+Pkg.build("PyCall")
+"""
+
 
 Random.seed!(1234) 
 
-
-T = 12
-M = 4
-E = [3 for _ in 1:T]
-d = [rand(20:70) for _ in 1:T]
-f = [10, 30, 60, 90]
-e = [8, 6, 4, 2]
-h = [1 for _ in 1:T]
-p = zeros(Int, T, M)
-
+#function data()
+global T = 12
+global M = 4
+global E = [3 for _ in 1:T]
+global d = [rand(20:70) for _ in 1:T]
+global f = [10, 30, 60, 90]
+global e = [8, 6, 4, 2]
+global h = [1 for _ in 1:T]
+global p = zeros(Int, T, M)
+#end
 
 
 """
-data : M, T, p, f, h, d, r, e, E
+parameter data : M, T, p, f, h, d, r, e, E
 """
 function cplexSolve(r::Int64)
     model = Model(CPLEX.Optimizer) 
@@ -31,7 +39,7 @@ function cplexSolve(r::Int64)
     )
 
     @constraint(model, [t in 2:T],
-         sum(x[t, :])  - s[t] + s[t-1] == d[t]
+        sum(x[t, :])  - s[t] + s[t-1] == d[t]
     )
 
     @constraint(model, sum(x[1, :])  - s[1] == d[1])
@@ -45,12 +53,13 @@ function cplexSolve(r::Int64)
         sum( (e[m] - E[t_]) * x[t_, m] for t_ in t:t+r, m in 1:M) <= 0
     )
 
+    set_silent(model) # turn off cplex output
 
     # solve the problem
     optimize!(model)
 
     exploredNodes = MOI.get(backend(model), MOI.NodeCount())
-    solveTime = MOI.get(model, MOI.SolveTime())
+    solveTime = round(MOI.get(model, MOI.SolveTime()), digits = 2)
 
     # status of model
     status = termination_status(model)
@@ -60,18 +69,19 @@ function cplexSolve(r::Int64)
     println("solveTime : ", solveTime)
 
     if isOptimal
-        total_cost = objective_value(model)
+        total_cost = round(objective_value(model), digits = 2)
         println("total_cost = ", total_cost)
-        @show JuMP.value.(x)
+        # @show JuMP.value.(x)
         x_star = JuMP.value.(x)
 
-        mean_emission_carbon = sum(e[m] * x_star[t,m] for t in 1:T, m in 1:M) / sum(x_star)
+        mean_emission_carbon = round(sum(e[m] * x_star[t,m] for t in 1:T, m in 1:M) / sum(x_star), digits = 2)
         println(" mean_emission_carbon = ", mean_emission_carbon)
-
+        return total_cost, mean_emission_carbon, solveTime
+    else
+        error("cplex doesn't find the optimal solution !")
     end
 
 end
-
 
 function test()
     r = 3
@@ -80,7 +90,40 @@ end
 
 
 function analysis_r()
-    # for r in 1:
-        
-    # end
+    global d
+    global T
+
+    for test in 1:4
+        seed = rand(1:10000)
+        Random.seed!(seed) 
+        d = [rand(20:70) for _ in 1:T]
+        # @show d
+
+        record_r = []
+        record_cost = []
+        record_emission = []
+        record_times = []
+        for r in 1:T
+            total_cost, mean_emission_carbon, solveTime = cplexSolve(r)
+            @show total_cost, mean_emission_carbon, solveTime
+            append!(record_r, r)
+            append!(record_cost, total_cost)
+            append!(record_emission, mean_emission_carbon)
+            append!(record_times, solveTime)
+        end
+
+        fig, ax = plt.subplots()
+        ax.plot(record_r, record_cost, color="red", marker="o", linewidth=2.0, linestyle="--")
+        ax.set_xlabel("L'intervalle r", fontsize=14)
+        ax.set_ylabel("Coût total", color="red", fontsize=14)
+        ax.set_title("L'évolution du coût total et l'émission carbone moyenne", fontsize=14)
+
+        ax2=ax.twinx()
+        ax2.plot(record_r, record_emission, color="blue", marker="o", linewidth=2.0, linestyle="--")
+        ax2.set_ylabel("L'émission carbone moyenne", color="blue", fontsize=14)
+        # plt.show()
+
+        savefig("analysis_r_test$test" * ".png")
+        # plt.close()
+    end
 end
