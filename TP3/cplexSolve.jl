d9c8c84a7229bed1ee04ad6d93624dd0b215f581
Yue0925 <mellet1997@outlook.com>
2022-03-25 12:08:43 +0100

    yesterday work

--- TP3/cplexSolve.jl
+++ TP3/cplexSolve.jl
@@ -2,9 +2,13 @@ TOL = 0.00001
 
 using Random
 using PyPlot
+import PyPlot; const plt = PyPlot
 
+
+global exploredNodes, solveTime, LB, E
 
 function cplexSOlve()
+    global exploredNodes, solveTime, LB, E
 
     M = Model(CPLEX.Optimizer) 
 
@@ -22,15 +26,18 @@ function cplexSOlve()
     @constraint(M, sum(x[i] for i in 1:Nm) == N)
     @constraint(M, sum(x[i] for i in N-Nf+1:N) == N)
 
-    @constraint(M, [i in 1:N], x[i] <= 2)
+    @constraint(M, [i in 1:N], x[i] <= 3)
 
     @objective(M, Min, sum(p))
 
     # solve the problem
+    set_silent(M) # turn off cplex output
     optimize!(M)
 
     exploredNodes = MOI.get(backend(M), MOI.NodeCount())
-    solveTime = MOI.get(M, MOI.SolveTime())
+    println("exploredNodes : ", exploredNodes)
+    solveTime = round(MOI.get(M, MOI.SolveTime()), digits = 3)
+    println("solveTime : ", solveTime)
 
     # status of model
     status = termination_status(M)
@@ -38,24 +45,34 @@ function cplexSOlve()
 
     println("isOptimal ? ", isOptimal)
     if isOptimal
-        println("x = ", value.(M[:x]))
+        println("x = ", round.(Int, value.(M[:x])))
 
-        println("Lower bound = ", objective_value(M))
+        println("Lower bound = ", round(objective_value(M), digits = 6))
+        LB = round(objective_value(M), digits = 6)
 
-        println("Log probabilites : ", value.(M[:p]))
+        println("Log probabilites : ", round.(value.(M[:p]), digits = 6))
 
         proba = zeros(G, A)
         for g in 1:G
             for a in 1:A
                 if JuMP.value(p[g, a]) > TOL
-                    proba[g, a] = (0.5)^(sum(JuMP.value(x[i]) for i in 1:N if sum(individu[i][1][g])%2==1))
+                    tmp = [JuMP.value(x[i]) for i in 1:N if sum(individu[i][1][g])%2==1]
+                    if size(tmp, 1) == 0
+                        proba[g, a] = 1
+                    else
+                        proba[g, a] = (0.5)^(sum(JuMP.value(x[i]) for i in 1:N if sum(individu[i][1][g])%2==1))
+                    end
+                    
                 end
             end
         end
 
-        println("Real probability = ",proba)
+        println("Real probability = ", round.(proba, digits = 6))
 
-        println("True expected value = ", sum(proba))
+        println("True expected value = ", round(sum(proba), digits = 6))
+        E = round(sum(proba), digits = 6)
+    else
+        error("cplex cannot find opt sol !")
     end
 
 end
@@ -64,5 +81,101 @@ end
 function run()
     include("DivGenetique_opl.dat")
     cplexSOlve()
+
+end
+
+
+function analysis_picewises()
+    include("DivGenetique_opl.dat")
+    global H
+    global exploredNodes, solveTime, LB, E
+
+    fout = open("res/picewises.txt", "w")
+
+    record_H = []
+    record_E = []
+    record_LB = []
+
+    for pw in 1:20
+        H = pw * 10
+        println("H = ", H)
+        cplexSOlve()
+
+        append!(record_H, H)
+        append!(record_LB, LB)
+        append!(record_E, E)
+
+        println(fout, H, " & ", exploredNodes, " & ", solveTime, " & ", LB, " & ", E, " \\\\")
+    end
+
+    close(fout)
+
+    plt.plot(record_H, record_LB, label = "LB", marker="o", linewidth=1.0, linestyle="--")
+    plt.plot(record_H, record_E, label = "E", marker="o", linewidth=1.0, linestyle="--")
+
+
+    plt.legend(loc="lower right", fontsize=8)
+    title("Impact of the number of picewises", fontsize=10)
+    xlabel("number of picewises", fontsize=10)
+    ylabel("Approximated / admissible expected value", fontsize=10)
+    savefig("res/picewises.png")
+    plt.close()
+
+end
+
+
+
+function analysis_population()
+    include("DivGenetique_opl.dat")
+    global exploredNodes, solveTime, LB, E
+    global N, Nm, Nf, individu, G
+
+    fout = open("res/population_size.txt", "w")
+
+    for pop in 1:5
+        N = 2 * pop
+        Nm = pop
+        Nf = pop
+
+        println("N = ", N)
+        seed = rand(1:10000)
+        Random.seed!(seed) 
+        individu = [ [[ [rand(1:2), rand(1:2)] for _ in 1:G]] for i in 1:N]
+
+        cplexSOlve()
+
+        println(fout, N, " & ", exploredNodes, " & ", solveTime, " & ", LB, " & ", E, " \\\\")
+
+    end
+
+    close(fout)
+
+end
+
+
+function analysis_genes()
+    include("DivGenetique_opl.dat")
+    global exploredNodes, solveTime, LB, E
+    global N, individu, G
+
+    fout = open("res/genes_number.txt", "w")
+
+    for gene in 2:15
+        G = gene
+
+        println("G = ", G)
+
+        seed = rand(1:10000)
+        Random.seed!(seed) 
+        individu = [ [[ [rand(1:2), rand(1:2)] for _ in 1:G]] for i in 1:N]
+
+        cplexSOlve()
+
+        println(fout, G, " & ", exploredNodes, " & ", solveTime, " & ", LB, " & ", E, " \\\\")
+
+    end
+
+    close(fout)
+
 
 end
